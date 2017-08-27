b80020ac84be946be105abdd58d3eab224f8cb67
Ivan Jureša <ivan.juresa@rimac-automobili.com>
2017-08-27 17:41:46 +0200

    Workign getWifiInfo Dialog

--- NRM/app/src/main/java/com/example/jura611/nrm/MainActivity.java
+++ NRM/app/src/main/java/com/example/jura611/nrm/MainActivity.java
@@ -12,6 +12,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -61,16 +62,9 @@ public class MainActivity extends AppCompatActivity {
         gButtonStartScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("gButtonStartScan", "Usao je u onClick");
                 // When starting, check if mobile is NOT connected to the WIFI
                 if(!isConnectedWifi()) {
-                    Log.i("gButtonStartScan", "Usao je u !isConnectedWifi");
-                    // Create popup to offer WIFIs
-                    final Dialog mDialog = new Dialog(MainActivity.this);
-                    mDialog.setContentView(R.layout.custom_dialog);
-                    mDialog.setTitle("test");
-
-                    //mScanResults. = (ListView) mDialog.findViewById(R.id.dialogListView);
+                    Toast.makeText(MainActivity.this, "Connect to Wifi", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -79,18 +73,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(isConnectedWifi()) {     //!< WIFI is connected
-                    Log.i("gButtonShowDetails", "Usao je u !isConnectedWifi");
 
                     // Create popup to show details of the WIFI network
                     getConnectedWifiInfo();
 
+                    // When Broadcast receiver didn't fire
                     if(mScanResults == null) {
 
                     }
                     else {
+                        String ssid =  null;
                         for(position = 0; position < mScanResults.size(); position ++) {
-                            Log.d(wifiInfo.getSSID() + " = " + mScanResults.get(position).SSID, "ono");
-                            if(wifiInfo.getSSID() == mScanResults.get(position).SSID.toString()) {
+                            ssid = getWifiSSID(mWifiManager);
+
+                            if(mScanResults.get(position).SSID.equals(ssid)) {
                                 Log.d(wifiInfo.getBSSID() + " = " + mScanResults.get(position).SSID, "ono");
                                 break;
                             }
@@ -106,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
     private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i("Usao je", "da");
             if(intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
                 mScanResults = mWifiManager.getScanResults();
                 Log.d(String.valueOf(mScanResults.size()), "Size of and array");
@@ -131,6 +126,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void getConnectedWifiInfo() {
         wifiInfo = mWifiManager.getConnectionInfo();
+    }
+
+    private String getWifiSSID(WifiManager _wifiManager) {
+        WifiInfo _wifiInfo = _wifiManager.getConnectionInfo();
+
+        return _wifiInfo.getSSID().replaceAll("\"","");
     }
 }
 
